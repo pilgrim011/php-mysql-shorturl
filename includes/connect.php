5d35227d2f2d31e036981318bc046e0e5aa95791
pilgrim011 <printerstampac@gmail.com>
2017-05-02 04:54:10 +0200

    and more env changes, getting boring...

--- includes/connect.php
+++ includes/connect.php
@@ -8,7 +8,7 @@ $db = substr($url["path"], 1);
 
 try
 {
-$pdo = new PDO($server;$db,
+$pdo = new PDO("$server;$db",
 $username, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->exec('SET NAMES "utf8"');
