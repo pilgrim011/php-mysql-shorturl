bce2687517765869c9fa5467af74d9e163f7e335
pilgrim011 <printerstampac@gmail.com>
2017-05-02 04:38:42 +0200

    env var

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,15 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
 try
 {
-$pdo = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_503a03e10b4d793',
-'DB_USERNAME', 'DB_PASSWORD');
+$pdo = new PDO('mysql:host=$server;dbname=$db',
+'$username', '$password');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->exec('SET NAMES "utf8"');
 }
